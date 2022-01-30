6f12cedcf09f3c8bc725f9be67f5dfc651e7d2be
Marcelo Destefani <marcelo@hastapronto.cl>
2022-01-31 00:26:39 +0100

    get book example

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
 from bibliothek import Library, Librarian
 
+
+
+# Instantiate a librarian
 librarian = Librarian('the_library')
-librarian.get_book('superarboribusco00giov')
+
+# Request book
+book_id = 'superarboribusco00giov'
+book_data = librarian.book_data(book_id)
+print(book_data)
+print(type(book_data))
+print(book_data['identifier'])
+
+book = librarian.get_book(book_data)
+print(book)
+print(type(book))
+
+
+
+
+
+
+
+# librarian.get_book('superarboribusco00giov')
+
+
+
