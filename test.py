67d85f59082ae152f787cbfd9a4f15be35be1ade
Marcelo Destefani <50741878+destefani@users.noreply.github.com>
2022-01-31 12:15:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ librarian = Librarian('the_library')
 # Request book
 book_id = 'superarboribusco00giov'
 book_data = librarian.book_data(book_id)
+
 print(book_data)
 print(type(book_data))
 print(book_data['identifier'])
@@ -14,14 +15,3 @@ print(book_data['identifier'])
 book = librarian.get_book(book_data)
 print(book)
 print(type(book))
-
-
-
-
-
-
-
-# librarian.get_book('superarboribusco00giov')
-
-
-
