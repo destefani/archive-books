625c84d3720c724fef3ed696c68b84759d22e6f4
Marcelo Destefani <marcelo@hastapronto.cl>
2022-03-09 13:08:15 +0100

    bunch

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-from bibliothek import Library, Librarian
+from bibliothek import Library, Librarian, Book
 
 
 # Instantiate a librarian
-librarian = Librarian('the_library')
+
+library_name = 'the_library'
+
+librarian = Librarian(library_name)
+
+
 
 # Request book
 book_id = 'superarboribusco00giov'
@@ -11,17 +16,11 @@ print(book_data)
 print(type(book_data))
 print(book_data['identifier'])
 
-book = librarian.get_book(book_data)
-print(book)
-print(type(book))
+book = Book(book_data, library_name)
+print(book.book_id)
+print(book.book_directory)
 
 
-
-
-
-
-
-# librarian.get_book('superarboribusco00giov')
-
-
-
+# book = librarian.get_book(book_data)
+# print(book)
+# print(type(book))
