b5e299c4aa1cf05927cf22e7dbd6c7a94791483c
Marcelo Destefani <marcelo@hastapronto.cl>
2022-03-12 12:38:12 +0100

    format

--- download_book.py
+++ download_book.py
@@ -1,18 +1,21 @@
 import click
 from bibliothek import Library
 
-
 #----------------------------------------------------------------------------
+
 @click.command()
 
 # Required
 @click.option('--library',   help='Library name',     type=str,   required=True)
 @click.option('--book-id',   help='Book identifier',  type=str,   required=True)
+
+#----------------------------------------------------------------------------
   
 def main(**kwargs):
     library = Library()
     library.load_library(kwargs['library'])
     library.download_book(kwargs['book_id'])
+
 #----------------------------------------------------------------------------
 
 if __name__ == "__main__":
